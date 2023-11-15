a7470ebfa3bd953ee9ee789a26fba9eb97774292
veeso <christian.visintin@veeso.dev>
2023-11-15 18:16:40 +0100

    feat: analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -10,6 +10,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang={'it'}>
+      <head>
+        <script
+          async
+          src="https://analytics.veeso.dev/script.js"
+          data-website-id="b4343ca0-5b4d-425b-984a-3aad3b411a02"
+        ></script>
+      </head>
       <body>
         <Topbar />
         <main>{children}</main>

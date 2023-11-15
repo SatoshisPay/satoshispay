bbcf9a5c347e3c30432f67cff12479dd6e32ccb6
veeso <christian.visintin@veeso.dev>
2023-11-15 18:21:15 +0100

    feat: script

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 
 import Footer from '@/src/js/components/Footer';
 import Topbar from '@/src/js/components/Topbar';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -10,13 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang={'it'}>
-      <head>
-        <script
-          async
-          src="https://analytics.veeso.dev/script.js"
-          data-website-id="b4343ca0-5b4d-425b-984a-3aad3b411a02"
-        ></script>
-      </head>
+      <Script
+        async
+        src="https://analytics.veeso.dev/script.js"
+        data-website-id="b4343ca0-5b4d-425b-984a-3aad3b411a02"
+      />
       <body>
         <Topbar />
         <main>{children}</main>

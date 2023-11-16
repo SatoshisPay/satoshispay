f9700a7638db539a1aaabd2738f801cafc673a61
veeso <christian.visintin@veeso.dev>
2023-11-16 15:44:12 +0100

    feat: alert commissioni

---
+++ src/components/reusable/Alerts.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { X, Info as InfoIcon, AlertTriangle } from 'react-native-feather';
+
+import { View } from 'react-native';
+
+const Danger = (props: React.HTMLProps<HTMLDivElement>) => (
+  <View
+    className={`${props.className} p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 shadow-lg`}>
+    <View className="flex flex-row items-center gap-8">
+      <View>
+        <X width={32} height={32} />
+      </View>
+      <View>{props.children}</View>
+    </View>
+  </View>
+);
+
+const Info = (props: React.HTMLProps<HTMLDivElement>) => (
+  <View
+    className={`${props.className} p-4 mb-4 text-sm text-blue-800 bg-blue-50 rounded-lg shadow-lg`}>
+    <View className="flex flex-row items-center gap-8">
+      <View>
+        <InfoIcon width={32} height={32} />
+      </View>
+      <View>{props.children}</View>
+    </View>
+  </View>
+);
+
+const Warning = (props: React.HTMLProps<HTMLDivElement>) => (
+  <View
+    className={`${props.className} p-4 mb-4 text-sm text-yellow-800 bg-yellow-50 rounded-lg shadow-lg`}>
+    <View className="flex flex-row items-center">
+      <AlertTriangle className="text-yellow-800 mx-4" width={32} height={32} />
+      <View>{props.children}</View>
+    </View>
+  </View>
+);
+
+export default {
+  Danger,
+  Info,
+  Warning,
+};
